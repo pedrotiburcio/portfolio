1f75d6ad7b1103e8508d095f2b170352909833db
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2023-08-20 19:56:24 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/pedrotiburcio.png" alt="" />
+    <Titulo fontSize={20}>Pedro Tibúrcio</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,19 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non veniam vitae
+      magni in expedita. Nam, dolores itaque veniam nesciunt inventore iusto
+      facilis ab omnis cum ex eos consectetur earum voluptates!
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=pedrotiburcio&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=pedrotiburcio&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
